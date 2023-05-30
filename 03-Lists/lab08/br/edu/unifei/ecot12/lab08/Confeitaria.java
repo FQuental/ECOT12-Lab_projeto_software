04df9cb82fa4a6fd9ba4e3195da78493d7c3c6b6
FQuental <felipedoquental@gmail.com>
2023-05-29 23:46:52 -0300

    lab08_incompleto

---
+++ 03-Lists/lab08/br/edu/unifei/ecot12/lab08/Confeitaria.java
@@ -0,0 +1,9 @@
+package 03-Lists.lab08.br.edu.unifei.ecot12.lab08;
+
+public class Confeitaria {
+    public Salgado novoSalgado();
+
+    
+
+
+}
---
+++ 03-Lists/lab08/br/edu/unifei/ecot12/lab08/Esfirraria.java
@@ -0,0 +1,9 @@
+package 03-Lists.lab08.br.edu.unifei.ecot12.lab08;
+
+public class Esfirraria extends Confeitaria {
+    
+    public Salgado novoSalgado(){
+        return new Coxinha();
+    }
+
+}
